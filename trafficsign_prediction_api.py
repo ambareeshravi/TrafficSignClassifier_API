8628057439ee70f6d827abf931071e9b6539bd5b
Ambareesh Ravi <ambareeshvichuravi@hotmail.com>
2021-08-02 14:41:59 -0400

    Update trafficsign_prediction_api.py

--- trafficsign_prediction_api.py
+++ trafficsign_prediction_api.py
@@ -54,4 +54,5 @@ if __name__ == '__main__':
     tester = Tester()
     # Run the application
     app.run(host='127.0.0.1', debug=True, port = 12345)
-#     app.run(host='192.168.1.184', debug=True, threaded=True, use_reloader=False, port = 8889, ssl_context='adhoc')
+    # To access externally
+    # app.run(host='<ip address>', debug=True, threaded=True, use_reloader=False, port = <port>, ssl_context='adhoc')
